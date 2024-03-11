46f1916e703804f1aaa7805151446c11a82dac73
Jules Lehodey <juleslehodey31320@hotmail.fr>
2024-03-11 16:52:09 +1100

    biomass computation : draft

--- seapodym_lmtl_python/post_production/biomass.py
+++ seapodym_lmtl_python/post_production/biomass.py
@@ -3,41 +3,49 @@ This module contains the post-production function used to compute the biomass.
 They are run after the production process.
 """
 
-from typing import Callable
+import numpy as np
 import xarray as xr
-from dask.distributed import Client
+from numba import jit
+
+from seapodym_lmtl_python.configuration.no_transport.labels import (
+    ConfigurationLabels,
+    PreproductionLabels,
+    ProductionLabels,
+)
 
 
-def sum_recruitment(recruited: xr.DataArray) -> xr.DataArray:
+@jit
+def biomass_sequence(recruited: np.ndarray, mortality: np.ndarray) -> np.ndarray:
     """
-    Sum the recruitment over the functional_group axis.
+    Compute the biomass of the recruited individuals.
 
-    Input
-    -----
-    - recruited [functional_group, time, latitude, longitude, cohort_age]
-
-    Output
-    ------
-    - recruitment [functional_group, time, latitude, longitude]
-
+    Formula :
+        B(t) = R(t) + (M(t) * B(t-1))
     """
+    biomass = np.zeros(recruited.shape)
+    biomass[:, 0, ...] = recruited[:, 0, ...]
+    # TODO(Jules) : Add initialisation from configuration
+    for timestep in range(1, recruited.shape[1]):
+        # TODO(Jules) : Check how to manage the ageing of aggregated cohorts
+        # -> For now, we consider that the mortality is the same for all cohorts because mortality is implicite to
+        # pre-production. We can use the sum of all cohorts to compute the production.
+        biomass[:, timestep, ...] = recruited[:, timestep, ...] + (
+            mortality[:, timestep, ...] * biomass[:, timestep - 1, ...]
+        )
+    return biomass
 
 
-def compute_biomass(
-    recruitment: xr.DataArray,
-    cell_area: xr.DataArray,
-    mortality: xr.DataArray,
-) -> xr.DataArray:
-    """
-    Compute the recruited biomass.
-
-    Input
-    -----
-    - recruitment [functional_group, time, latitude, longitude] from the `sum_recruitment()` function.
-    - cell_area [latitude, longitude]
-    - mortality [functional_group, time, latitude, longitude] from the `compute_mortality_field()` function.
-
-    Output
-    ------
-    - biomass [functional_group, time{select(timestep)}, latitude, longitude]
-    """
+def compute_biomass(data: xr.Dataset) -> xr.DataArray:
+    data = data.cf.transpose(ConfigurationLabels.fgroup, "T", "Y", "X", "Z", ConfigurationLabels.cohort)
+    recruited = data[ProductionLabels.recruited].sum(ConfigurationLabels.cohort)
+    recruited = np.nan_to_num(recruited.data, 0)
+    mortality = np.nan_to_num(data[PreproductionLabels.mortality_field].data, 0)
+    biomass = biomass_sequence(recruited, mortality)
+    return xr.DataArray(
+        biomass,
+        coords=data[PreproductionLabels.mortality_field].coords,
+        dims=data[PreproductionLabels.mortality_field].dims,
+        attrs={
+            # TODO(Jules)
+        },
+    )
